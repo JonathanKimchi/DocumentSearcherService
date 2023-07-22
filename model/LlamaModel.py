dca11e691a977c2ca1a07849e8e649f1ad52a0be
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2023-07-22 14:30:18 -0400

    Bumping token count size

--- model/LlamaModel.py
+++ model/LlamaModel.py
@@ -11,7 +11,7 @@ class LlamaModel(BaseModel):
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super().__new__(cls)
-            cls._instance.llm = LlamaCpp(model_path='model_data/llama-2-13b-chat.ggmlv3.q2_K.bin', n_ctx=1000, n_batch=8, callbacks=[], verbose=False)
+            cls._instance.llm = LlamaCpp(model_path='model_data/llama-2-13b-chat.ggmlv3.q2_K.bin', n_ctx=3000, n_batch=8, callbacks=[], verbose=False)
 
             cls._instance.tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")#type: ignore
 
