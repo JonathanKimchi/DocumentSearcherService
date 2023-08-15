7d7b28cbaa0063d4bef8bdca2fac1cd9c127d1db
JonathanKimchi <59551654+JonathanKimchi@users.noreply.github.com>
2023-08-14 23:33:24 -0400

    Adding authentication to endpoints

--- app.py
+++ app.py
@@ -1,15 +1,50 @@
 from dotenv import load_dotenv
-
-load_dotenv()
-
+import firebase_admin
+from firebase_admin import credentials, auth
+from functools import wraps
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from proxy.datebase_proxy import database_proxy
+import os
+
+load_dotenv()
+
+# Initialize Firebase Admin SDK
+
+cred = None
+
+if os.environ['ENV_STAGE'] != 'production':
+    credentials.Certificate('speakeasy-dev-c15db-firebase-adminsdk-fjtqq-c513c0b82a.json')
+else:
+    cert = os.environ['ENCODED_FIREBASE_CREDENTIALS']
+    cred = credentials.Certificate(cert)
+    
+firebase_admin.initialize_app(cred)
+
 
 app = Flask(__name__)
 CORS(app, resources={r"/data/*": {"origins": "*", "methods": "GET,HEAD,POST,OPTIONS,PUT,DELETE", "allow_headers": "*"}})
 
+# Authentication middleware
+def authenticate(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if token is None:
+            return jsonify({"message": "No token provided"}), 401
+        
+        try:
+            # Remove "Bearer " from the token
+            decoded_token = auth.verify_id_token(token.split(" ")[1])
+            request.user = decoded_token
+        except Exception as e:
+            return jsonify({"message": "Invalid token"}), 401
+        
+        return f(*args, **kwargs)
+    return wrapped
+
 @app.route('/data/get-chat-message', methods=['POST'])
+@authenticate
 def get_data():
     print("starting get_data")
     query = request.json.get('query')
@@ -23,6 +58,7 @@ def get_data():
     return jsonify(database_proxy.get_data(query))
 
 @app.route('/data/update-document', methods=['POST'])
+@authenticate
 def update_data():
     data = request.json.get('data')
     filename = request.json.get('filename')
